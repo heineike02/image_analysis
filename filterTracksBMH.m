63cb9276255043e00011a0bb34d6e4767b46620d
heineike02 <heineike02@gmail.com>
2014-04-23 18:38:52 -0700

    changes associated with bpac sorbitol experiment

--- filterTracksBMH.m
+++ filterTracksBMH.m
@@ -26,11 +26,32 @@ for jj = 1:nTracks
     end
 end
 
-
-
-
 tracks_out = tracks;
 tracks_out(nmi_filt) = [];
+
+%% This filter is for KL_M2MC data from20140416.  Position 3 had one NF point greater than 17 - it might be worth figuring out why that happened and tuning the algorithm
+nf_max = 17.0
+
+%Throw out tracks that have an average nf greater than nf_max,
+
+nTracks = length(tracks);
+mean_nf_vec = zeros(nTracks,1);
+
+for jj = 1: nTracks
+    mean_nf_vec(jj) = mean([tracks(jj).nf]);
+end
+
+nf_filt = [];
+for jj = 1:nTracks
+    if mean_nf_vec(jj)>nf_max
+        nf_filt = [nf_filt,jj];
+        ['threw out track ', int2str(jj), ' because mean nf of ',num2str(mean_nf_vec(jj)),' was greater than nf_max of ', num2str(nf_max)]
+    end
+end
+
+tracks_out = tracks;
+tracks_out(nf_filt) = [];
+
 
 
 
