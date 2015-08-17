5cda24f8ab3aed1d98d0876fcfc4ac16bda9b415
Susan Chen <susan.y.chen.24@gmail.com>
2015-08-17 15:53:00 -0700

    Updated extraction plotting template 1 by making the script a function

--- SYC_filter_extraction_plotting_SC_template_1.m
+++ SYC_filter_extraction_plotting_SC_template_1.m
@@ -1,7 +1,6 @@
-% SYC_data_plotting_FiveDifferentConditions
+function [] = SYC_filter_extraction_plotting_SC_template_1(ipdir, base_dir, phases, fname_load, well_order, perturbation)
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% OSMO
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This function:
 %     1) Filters single cell traces
 %     2) Extracts single cell features
@@ -11,26 +10,45 @@
 % experiment
 %
 % %%%% = places where you need to change the script
+%
+% Inputs to function:
+%       1. ipdir = directory where github image analysis code is stored
+%       2. base_dir = directory where the processed .mat file of single
+%       cell traces are stored (from previous processing scripts)
+%       3. phases = the "before" and "after" environmental conditions in a
+%       given experiment
+%       4. fname_load = filename of the processed .mat file of single cell
+%       traces
+%
+%
+% Outputs to function:
+%       there are no outputs
+%
+% Example of inputs:
+%       1. ipdir = '/Users/susanychen/Documents/image_analysis/';
+%       2. base_dir = '/Users/susanychen/Downloads/TempMatlabData20150808/';
+%       3. phases = {'Pre','Post'};
+%       4. fname_load= '20150709_SorbitolStress_ASOE_TRpanel_20150720';
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % set paths
 clear
 profile off
-%%%% ipdir = where your github code is located %%%%
-ipdir = '/Users/susanychen/Documents/image_analysis/';
-%ipdir = 'C:\Users\susanychen\GitHub\image_analysis\';
+ipdir = ipdir;
 %adds image analysis directory to path based on directry of day's analysis
 %code
 path(ipdir,path)
 
 %%%% base_dir = directory where single cell data is stored %%%%
-base_dir = '/Users/susanychen/Downloads/TempMatlabData20150808/';
+base_dir = base_dir;
 %base_dir = 'C:\Users\susanychen\Google Drive\UCSF Graduate Research (El-Samad)\DECODING PKA BY DOWNSTREAM EFFECTORS\20150710_PhosphateDeplet_ASOE_TRpanel\processed_data_20150720\';
 %%%% phases = subfolders in the same experiment %%%%
-phases =  {'Pre','Post'}; %,'Post_p2'}
+phases = phases; 
 %%%% fname_load = the .mat folder that contains the single cell data %%%%
-fname_load = '20150709_SorbitolStress_ASOE_TRpanel_20150720.mat';
+fname_load = fname_load;
 
-%% Filter out single cell traces that are too short
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Filter out single cell traces that are too short
 
 % load the mat files
 load([base_dir,fname_load],'all_times_vec','all_tracks_vec','posvec')
@@ -87,19 +105,20 @@ end
 
 % How many cells remain after initial filtering?
 %%%% ffolder_save = folder where filtered traces will be stored %%%%
-ffolder_save = '/Users/susanychen/Downloads/TempMatlabData20150808/';
+%ffolder_save = base_dir;
 %ffolder_save = 'C:\Users\susanychen\Google Drive\UCSF Graduate Research (El-Samad)\DECODING PKA BY DOWNSTREAM EFFECTORS\20150710_PhosphateDeplet_ASOE_TRpanel\processed_data_20150720\';
 %%%% fname_save = file name of filtered traces %%%%
-fname_save = '20150709_SorbitolStress_ASOE_TRpanel_20150720_filter.mat';
-save(strcat(ffolder_save, fname_save),'all_tracks_vec', 'all_times_vec','all_tracks_filt_vec', 'all_times_filt_vec','posvec', 'cell_num_filt_vec')
+%fname_save = [fname_load,'_filter.mat'];
+%save(strcat(ffolder_save, fname_save),'all_tracks_vec', 'all_times_vec','all_tracks_filt_vec', 'all_times_filt_vec','posvec', 'cell_num_filt_vec')
 
-%% Feature Extraction and (Optional) Smoothing
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Feature Extraction and (Optional) Smoothing
 %%%% folder where the filtered traces live %%%%
-ffolder_load = '/Users/susanychen/Downloads/TempMatlabData20150808/';
+%ffolder_load = '/Users/susanychen/Downloads/TempMatlabData20150808/';
 %ffolder_load = 'C:\Users\susanychen\Google Drive\UCSF Graduate Research (El-Samad)\DECODING PKA BY DOWNSTREAM EFFECTORS\20150710_PhosphateDeplet_ASOE_TRpanel\processed_data_20150720\';
 %%%% filename of filtered traces %%%%
-fname_load = '20150709_SorbitolStress_ASOE_TRpanel_20150720_filter.mat';
-load(strcat(ffolder_load, fname_load),'all_tracks_filt_vec', 'all_times_filt_vec','posvec')
+%fname_load_1 = [fname_load, 'filter.mat'];
+%load(strcat(ffolder_load, fname_load_1),'all_tracks_filt_vec', 'all_times_filt_vec','posvec')
 % clear old variables
 clear out_Features_cell;
 clear t_singlecell_cell; clear y_singlecell_cell;
@@ -209,26 +228,28 @@ end
     out_Features_cell(gg).hasPeakNum = ff;
 end
 
-%%%% TR_names_long =  the labels that go with each well %%%%
-TR_names_long = {'SYC1-67 Cad1 phosphate deplete SDC', 'SYC1-73 Com2', ...
-    'SYC1-70 crz1', 'SYC1-62 Dot6', ...
-    'SYC1-75 Maf1', 'SYC1-72 Msn2', ...
-    'SYC1-64 Yap1', 'SYC1-76 Stb3', ...
-    'SYC1-69 Sko1', 'SYC1-65 Rtg3', ...
-    'SYC1-71 Pho4', 'SYC1-68 Nrg2','SYC1-74 Msn4'};
-%%%% TR_names_short =  the labels that go with each well %%%%
-TR_names_short = {'Cad1', 'Com2', ...
-    'crz1', 'Dot6', ...
-    'Maf1', 'Msn2', ...
-    'Yap1', 'Stb3', ...
-    'Sko1', 'Rtg3', ...
-    'Pho4', 'Nrg2','Msn4'};
+% %%%% TR_names_long =  the labels that go with each well %%%%
+% TR_names_long = {'SYC1-67 Cad1 phosphate deplete SDC', 'SYC1-73 Com2', ...
+%     'SYC1-70 crz1', 'SYC1-62 Dot6', ...
+%     'SYC1-75 Maf1', 'SYC1-72 Msn2', ...
+%     'SYC1-64 Yap1', 'SYC1-76 Stb3', ...
+%     'SYC1-69 Sko1', 'SYC1-65 Rtg3', ...
+%     'SYC1-71 Pho4', 'SYC1-68 Nrg2','SYC1-74 Msn4'};
+% %%%% TR_names_short =  the labels that go with each well %%%%
+% TR_names_short = {'Cad1', 'Com2', ...
+%     'crz1', 'Dot6', ...
+%     'Maf1', 'Msn2', ...
+%     'Yap1', 'Stb3', ...
+%     'Sko1', 'Rtg3', ...
+%     'Pho4', 'Nrg2','Msn4'};
 %%%% stress_type = labels of stress type %%%%
-stress_type = {'20150710 sorbStress'};
+%stress_type = {'20150710 sorbStress'};
+well_order = well_order;
+perturbation = perturbation;
 
 %%%% fname_save = save plotting values/features %%%% -- stopped here
-fname_save = '/Users/susanychen/Downloads/TempMatlabData20150808/20150709_SorbitolStress_ASOE_TRpanel_20150720_filter.mat';
+fname_save = [base_dir,fname_load,'_filter.mat'];
 %fname_save = 'C:\Users\susanychen\Google Drive\UCSF Graduate Research (El-Samad)\DECODING PKA BY DOWNSTREAM EFFECTORS\20150710_PhosphateDeplet_ASOE_TRpanel\processed_data_20150720\20150710_PhosphateDeplet_ASOE_TRpanel_20150720_filter.mat';
 save([fname_save], 'all_tracks_vec', 'all_times_vec','all_tracks_filt_vec', 'all_times_filt_vec','posvec',...
     'out_Features_cell',...
-    'TR_names_long', 'TR_names_short','stress_type', 'cell_num_filt_vec')
+    'well_order', 'perturbation', 'cell_num_filt_vec')
